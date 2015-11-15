0e7eed33165cdc8cc922026d56e3fe4f92c8d018
jreadey <jreadey@hdfgroup.org>
2015-11-14 19:51:29 -0800

    add hdf5 version verification

--- testall.py
+++ testall.py
@@ -12,9 +12,26 @@
 
 import os
 import sys
+import h5py
 
 unit_tests = ( 'hdf5dtypeTest', 'hdf5dbTest' )
 integ_tests = ( 'h5tojson_test', 'jsontoh5_test' )
+
+# verify the hdf5 lib version is recent
+hdf5_version = h5py.version.hdf5_version_tuple
+print("hdf5_version:", hdf5_version)
+if hdf5_version[1] < 8:
+    sys.exit("Need hdf5 lib 1.8 or later")
+if hdf5_version[1] == 8 and hdf5_version[2] < 11:
+    sys.exit("Need hdf5 lib 1.8.11 or later")
+# verify we have a recent version of h5py
+print("h5py_version:", h5py_version)
+h5py_version = h5py.version.version_tuple
+print(h5py_version)
+if h5py_version[0] != 2 or h5py_version[1] < 5:
+    sys.exit("Need h5py version 2.5 or later")
+    
+#
 #
 # Run all hdf5-json tests
 # Run this script before running any integ tests
