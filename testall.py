20625f0945139c1e771e9608b455674c49cd265d
jreadey <jreadey@hdfgroup.org>
2016-02-10 20:28:49 -0800

    remove extra print statements

--- testall.py
+++ testall.py
@@ -19,7 +19,6 @@ integ_tests = ( 'h5tojson_test', 'jsontoh5_test' )
 print(h5py.version.info)
 # verify the hdf5 lib version is recent
 hdf5_version = h5py.version.hdf5_version_tuple
-print("hdf5_version:", hdf5_version)
 if hdf5_version[1] < 8:
     sys.exit("Need hdf5 lib 1.8 or later")
 if hdf5_version[1] == 8 and hdf5_version[2] < 4:
@@ -27,7 +26,6 @@ if hdf5_version[1] == 8 and hdf5_version[2] < 4:
 # verify we have a recent version of h5py
 
 h5py_version = h5py.version.version_tuple
-print("h5py_version:", h5py_version)
 if h5py_version[0] != 2 or h5py_version[1] < 5:
     sys.exit("Need h5py version 2.5 or later")
     
