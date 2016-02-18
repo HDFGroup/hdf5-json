c3affa3ab2c35721ae14ced11519fff93af8bfca
john <john>
2016-02-17 22:15:05 -0800

    fix test scripts for Windows

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy>=1.6.1', 'h5py>=2.5'],
+    install_requires=['numpy>=1.10.4', 'h5py>=2.5'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
--- test/integ/jsontoh5_test.py
+++ test/integ/jsontoh5_test.py
@@ -127,7 +127,7 @@ for test_file in test_files:
     out_file = os.path.join(out_dir, split_ext[0] + ".h5")
     if not os.path.exists(file_path):
         sys.exit("file: " + file_path + " not found")
-    cmd = "jsontoh5.py " + file_path + " " + out_file
+    cmd = "python ../../jsontoh5/jsontoh5.py " + file_path + " " + out_file
     print("cmd:", cmd)
     rc = os.system(cmd)
     if rc != 0:
