cf5adf169a4d9470cbf7a6abea6ab72b5b19d69d
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2021-04-23 20:21:36 -0400

    Update travis and setup with recent python versions

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -14,104 +15,82 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='h5json',
-
+    name="h5json",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.3',
-
-    description='HDF5/JSON Tools',
+    version="1.1.3",
+    description="HDF5/JSON Tools",
     long_description=long_description,
-
     # The project's main homepage.
-    url='https://github.com/HDFGroup/hdf5-json',
-
+    url="https://github.com/HDFGroup/hdf5-json",
     # Author details
-    author='John Readey',
-    author_email='jreadey@hdfgroup.org',
-
+    author="John Readey",
+    author_email="jreadey@hdfgroup.org",
     # Choose your license
-    license='BSD',
-
+    license="BSD",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
-
+        "Development Status :: 5 - Production/Stable",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-
     # What does your project relate to?
-    keywords='json hdf5 numpy array data',
-
+    keywords="json hdf5 numpy array data datacube",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    #packages=find_packages(exclude=['docs', 'test*']),
-    packages=['h5json','h5json.h5tojson','h5json.jsontoh5'],
-
+    # packages=find_packages(exclude=['docs', 'test*']),
+    packages=["h5json", "h5json.h5tojson", "h5json.jsontoh5"],
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     #   py_modules=["my_module"],
-
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy>=1.10.4', 'h5py>=2.5'],
-    setup_requires=['pkgconfig', 'six'],
+    python_requires="~=3.7",
+    install_requires=["numpy>=1.10.4", "h5py>=3.0"],
+    setup_requires=["pkgconfig", "six"],
     zip_safe=False,
-
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
     },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'h5json': ['data/json/*', 'data/hdf5/*']
-    },
-
+    package_data={"h5json": ["data/json/*", "data/hdf5/*"]},
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
+    # data_files=[('my_data', ['data/data_file'])],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={'console_scripts':
-          ['h5tojson = h5json.h5tojson.h5tojson:main', 
-           'jsontoh5 = h5json.jsontoh5.jsontoh5:main' 
-          ]},
+    entry_points={
+        "console_scripts": [
+            "h5tojson = h5json.h5tojson.h5tojson:main",
+            "jsontoh5 = h5json.jsontoh5.jsontoh5:main",
+        ]
+    },
 )
